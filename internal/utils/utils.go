0da7a10870593a99cbfd1229cbbaa5836cfe1fde
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-05-22 14:26:39 +0300

    added: edge case handling

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,11 +2,19 @@ package utils
 
 func SplitSlice(slice []int, batchSize int) [][]int {
 
-	if batchSize == 0 {
+	if batchSize <= 0 || slice == nil {
 		return nil
 	}
 
-	sliceOfBatches := make([][]int, len(slice)/batchSize+1)
+	var numberOfBatches int
+
+	if len(slice)%batchSize == 0 {
+		numberOfBatches = len(slice) / batchSize
+	} else {
+		numberOfBatches = len(slice)/batchSize + 1
+	}
+
+	sliceOfBatches := make([][]int, numberOfBatches)
 
 	for i := 0; i < len(slice); i++ {
 		sliceOfBatches[i/batchSize] = append(sliceOfBatches[i/batchSize], slice[i])
