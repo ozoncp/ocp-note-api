0592fc75cd37f2ff2f4044c3cb8f715c76d84531
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-05-22 15:53:12 +0300

    added test for func SplitSlice

--- internal/utils/test/utils_test.go
+++ internal/utils/test/utils_test.go
@@ -1,7 +1,10 @@
 package utils_test
 
-import ("fmt"
-		"github.com/ozoncp/ocp-note-api/internal/utils")
+import (
+	"fmt"
+
+	"github.com/ozoncp/ocp-note-api/internal/utils"
+)
 
 func AllTest() {
 	SplitSliceTest()
@@ -11,23 +14,73 @@ func SplitSliceTest() {
 
 	type Dataset struct {
 		BatchSize int
-		Input []int
-		Output [][]int
+		Input     []int
+		Output    [][]int
 	}
 
-	dataset := []Dataset {
-		{ 2, []int{} { 1, 2, 3, 4, 5, 6 }, [][]int{} { {1, 2}, {3, 4}, {5, 6} } }
+	dataset := []Dataset{
+		{
+			BatchSize: 2,
+			Input:     []int{1, 2, 3, 4, 5, 6},
+			Output:    [][]int{{1, 2}, {3, 4}, {5, 6}},
+		},
+		{
+			BatchSize: 0,
+			Input:     []int{1, 2, 3, 4, 5, 6},
+			Output:    nil,
+		},
+		{
+			BatchSize: -6,
+			Input:     []int{1, 2, 3, 4, 5, 6},
+			Output:    nil,
+		},
+		{
+			BatchSize: 200,
+			Input:     []int{1, 2, 3, 4, 5, 6},
+			Output:    [][]int{{1, 2, 3, 4, 5, 6}},
+		},
+		{
+			BatchSize: 6,
+			Input:     []int{1, 2, 3, 4, 5, 6},
+			Output:    [][]int{{1, 2, 3, 4, 5, 6}},
+		},
+		{
+			BatchSize: 2,
+			Input:     nil,
+			Output:    nil,
+		},
 	}
 
 	fmt.Println("*Split Slice Test*")
 
-	
-	for example := range dataset {
-		if utils.SplitSlice(example.Input, example.BatchSize) != example.Output {
-			fmt.Println("Test failed (Input: " + example.Input)
+	for _, example := range dataset {
+		result := utils.SplitSlice(example.Input, example.BatchSize)
+
+		if EqualSlices(result, example.Output) {
+			fmt.Printf("Test passed (Input: %v, output: %v, batchSize: %v)\n", example.Input, result, example.BatchSize)
 		} else {
-			fmt.Println("Test passed (Input: " + example.Input)
+			fmt.Printf("Test failed (Input: %v, expected output: %v, output: %v, batchSize: %v)\n", example.Input, example.Output, result, example.BatchSize)
+		}
+	}
+}
+
+func EqualSlices(first [][]int, second [][]int) bool {
+
+	if len(first) != len(second) {
+		return false
+	}
+
+	for i, slice := range first {
+		if len(slice) != len(second[i]) {
+			return false
+		}
+
+		for j, val := range slice {
+			if val != second[i][j] {
+				return false
+			}
 		}
 	}
 
+	return true
 }
