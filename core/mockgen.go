f74f6a67b0b8130c3aeb508e0b299c1ef29b524c
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-05-31 15:34:55 +0300

    changed: mockgen.go

--- core/mockgen.go
+++ core/mockgen.go
@@ -1,3 +1,3 @@
 package core
 
-//go:generate mockgen -destination=./mocks/repo_mock.go -package=mocks github.com/ozoncp/ocp-note-api/core/repo Repo
+//go:generate mockgen -destination=./mocks/flusher_mock.go -package=mocks github.com/ozoncp/ocp-note-api/core/flusher Flusher
