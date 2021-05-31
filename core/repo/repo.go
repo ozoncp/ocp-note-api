3a34f206e62bcae398582dac0fc9eb0c49a6850c
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-05-31 09:46:41 +0300

    added: new methods for interface repo

--- core/repo/repo.go
+++ core/repo/repo.go
@@ -4,4 +4,7 @@ import "github.com/ozoncp/ocp-note-api/core/note"
 
 type Repo interface {
 	AddNotes(notes []note.Note) error
+	DescribeNote(id uint64) (*note.Note, error)
+	ListNotes(count, offset uint64) ([]note.Note, error)
+	RemoveNote(id uint64) error
 }
