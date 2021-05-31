46c565be39e091ad9cab93bb058a950f7c12fcfb
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-05-31 15:59:21 +0300

    added: generate mock for repo

--- core/mockgen.go
+++ core/mockgen.go
@@ -1,3 +1,4 @@
 package core
 
 //go:generate mockgen -destination=./mocks/flusher_mock.go -package=mocks github.com/ozoncp/ocp-note-api/core/flusher Flusher
+//go:generate mockgen -destination=./mocks/repo_mock.go -package=mocks github.com/ozoncp/ocp-note-api/core/repo Repo
