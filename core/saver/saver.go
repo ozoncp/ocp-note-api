e7cced45a063ebae0a5dc113bc5c3e45af61c02c
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-06-02 09:56:34 +0300

    added: implementation of interface saver

--- core/saver/saver.go
+++ core/saver/saver.go
@@ -1,11 +1,32 @@
 package saver
 
 import (
+	"github.com/ozoncp/ocp-note-api/core/flusher"
 	"github.com/ozoncp/ocp-note-api/core/note"
 )
 
 type Saver interface {
-	Save(entity note.Note)
+	Save(note note.Note)
 	// Init()
 	Close()
+}
+
+type saver struct {
+	capacity uint
+	flusher  flusher.Flusher
+}
+
+func (s *saver) Save(note note.Note) {
+
+}
+
+func (s *saver) Close() {
+
+}
+
+func New(capacity uint, flusher flusher.Flusher) Saver {
+	return &saver{
+		capacity: capacity,
+		flusher:  flusher,
+	}
 }
