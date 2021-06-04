2053464ee07b1f31f2253da7fb4b9b1c43cd9d3e
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-06-04 22:43:53 +0300

    added: test file for saver

---
+++ core/saver/saver_test.go
@@ -0,0 +1,12 @@
+package saver_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/ozoncp/ocp-note-api/core/saver"
+)
+
+var _ = Describe("Saver", func() {
+
+})
