28fc327c9a7093ca937042f798d7fcec54153333
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-06-12 15:48:36 +0300

    changed: impl. handle CreateNoteV1

--- core/api/api.go
+++ core/api/api.go
@@ -3,6 +3,8 @@ package api
 import (
 	"context"
 
+	"github.com/ozoncp/ocp-note-api/core/note"
+	"github.com/ozoncp/ocp-note-api/core/repo"
 	desc "github.com/ozoncp/ocp-note-api/pkg/ocp-note-api"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -10,6 +12,11 @@ import (
 
 type api struct {
 	desc.UnimplementedOcpNoteApiServer
+	repo repo.Repo
+}
+
+func NewOcpNoteApi(repo repo.Repo) desc.OcpNoteApiServer {
+	return &api{repo: repo}
 }
 
 func init() {
@@ -17,9 +24,29 @@ func init() {
 }
 
 func (a *api) CreateNoteV1(ctx context.Context, request *desc.CreateNoteV1Request) (*desc.CreateNoteV1Response, error) {
-	log.Print("Create note ", request)
+	log.Print("Create note ...")
 
-	return nil, nil
+	if err := request.Validate(); err != nil {
+		log.Error().Err(err).Msg("invalid argument")
+		return nil, err
+	}
+
+	note := &note.Note{
+		UserId:      request.UserId,
+		ClassroomId: request.ClassroomId,
+		DocumentId:  request.DocumentId,
+	}
+
+	noteId, err := a.repo.AddNote(ctx, note)
+
+	if err != nil {
+		log.Error().Err(err).Msg("failed to create note")
+		return nil, err
+	}
+
+	log.Info().Msgf("Create note success (id: %d)", noteId)
+
+	return &desc.CreateNoteV1Response{NoteId: noteId}, nil
 }
 
 func (a *api) DescribeNoteV1(ctx context.Context, request *desc.DescribeNoteV1Request) (*desc.DescribeNoteV1Response, error) {
@@ -38,8 +65,4 @@ func (a *api) RemoveNoteV1(ctx context.Context, request *desc.RemoveNoteV1Reques
 	log.Print("Remove note")
 
 	return nil, nil
-}
-
-func NewOcpNoteApi() desc.OcpNoteApiServer {
-	return &api{}
 }
