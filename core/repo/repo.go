ad5a1d345f6126f092c81a7df2d61ca856830d57
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-06-12 14:21:19 +0300

    added: stub impl. of Repo interface

--- core/repo/repo.go
+++ core/repo/repo.go
@@ -1,10 +1,43 @@
 package repo
 
-import "github.com/ozoncp/ocp-note-api/core/note"
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/ozoncp/ocp-note-api/core/note"
+)
 
 type Repo interface {
-	AddNotes(notes []note.Note) error
-	DescribeNote(id uint64) (*note.Note, error)
-	ListNotes(count, offset uint64) ([]note.Note, error)
-	RemoveNote(id uint64) error
+	AddNotes(ctx context.Context, notes []note.Note) error
+	DescribeNote(ctx context.Context, id uint64) (*note.Note, error)
+	ListNotes(ctx context.Context, count, offset uint64) ([]note.Note, error)
+	RemoveNote(ctx context.Context, id uint64) error
+}
+
+const (
+	tableName = "notes"
+)
+
+type repo struct {
+	db sqlx.DB
+}
+
+func New(db sqlx.DB) Repo {
+	return &repo{db: db}
+}
+
+func (r *repo) AddNotes(ctx context.Context, notes []note.Note) error {
+	return nil
+}
+
+func (r *repo) DescribeNote(ctx context.Context, id uint64) (*note.Note, error) {
+	return nil, nil
+}
+
+func (r *repo) ListNotes(ctx context.Context, count, offset uint64) ([]note.Note, error) {
+	return nil, nil
+}
+
+func (r *repo) RemoveNote(ctx context.Context, id uint64) error {
+	return nil
 }
