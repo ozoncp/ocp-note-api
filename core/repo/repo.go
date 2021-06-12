fe9619bb16c4e6d34f2ed6cf7a049d21343d0995
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-06-12 14:44:26 +0300

    added: impl methods AddTask and AddTasks

--- core/repo/repo.go
+++ core/repo/repo.go
@@ -3,11 +3,13 @@ package repo
 import (
 	"context"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/jmoiron/sqlx"
 	"github.com/ozoncp/ocp-note-api/core/note"
 )
 
 type Repo interface {
+	AddNote(ctx context.Context, note note.Note) (uint64, error)
 	AddNotes(ctx context.Context, notes []note.Note) error
 	DescribeNote(ctx context.Context, id uint64) (*note.Note, error)
 	ListNotes(ctx context.Context, count, offset uint64) ([]note.Note, error)
@@ -26,8 +28,32 @@ func New(db sqlx.DB) Repo {
 	return &repo{db: db}
 }
 
+func (r *repo) AddNote(ctx context.Context, note note.Note) (uint64, error) {
+	query := sq.Insert(tableName).
+		Columns("user_id", "classroom_id", "document_id").
+		Values(note.UserId, note.ClassroomId, note.DocumentId).
+		Suffix("RETURNING \"id\"").
+		RunWith(r.db).
+		PlaceholderFormat(sq.Dollar)
+
+	query.QueryRowContext(ctx).Scan(&note.Id)
+
+	return note.Id, nil
+}
+
 func (r *repo) AddNotes(ctx context.Context, notes []note.Note) error {
-	return nil
+	query := sq.Insert(tableName).
+		Columns("user_id", "classroom_id", "document_id").
+		RunWith(r.db).
+		PlaceholderFormat(sq.Dollar)
+
+	for _, note := range notes {
+		query = query.Values(note.UserId, note.ClassroomId, note.DocumentId)
+	}
+
+	_, err := query.ExecContext(ctx)
+
+	return err
 }
 
 func (r *repo) DescribeNote(ctx context.Context, id uint64) (*note.Note, error) {
