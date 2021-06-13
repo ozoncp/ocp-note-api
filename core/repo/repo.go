0c9ee9ba6c491225d9b80316bafb05bfb9d01288
Oleg Kozyrev <oleg.kozyrev@spbpu.com>
2021-06-13 18:15:48 +0300

    added: new method UpdateNote

--- core/repo/repo.go
+++ core/repo/repo.go
@@ -12,6 +12,7 @@ import (
 type Repo interface {
 	AddNote(ctx context.Context, note *note.Note) (uint64, error)
 	AddNotes(ctx context.Context, notes []note.Note) (uint64, error)
+	UpdateNote(ctx context.Context, notes *note.Note) (uint64, error)
 	DescribeNote(ctx context.Context, id uint64) (*note.Note, error)
 	ListNotes(ctx context.Context, limit, offset uint64) ([]note.Note, error)
 	RemoveNote(ctx context.Context, id uint64) error
@@ -69,6 +70,32 @@ func (r *repo) AddNotes(ctx context.Context, notes []note.Note) (uint64, error) 
 	}
 
 	return uint64(rowsAffected), err
+}
+
+func (r *repo) UpdateNote(ctx context.Context, note *note.Note) (uint64, error) {
+	query := sq.Update(tableName).
+		Set("user_id", note.UserId).
+		Set("classroom_id", note.ClassroomId).
+		Set("document_id", note.DocumentId).
+		Where(sq.Eq{"id": note.Id})
+
+	result, err := query.ExecContext(ctx)
+
+	if err != nil {
+		return uint64(0), err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+
+	if err != nil {
+		return uint64(0), err
+	}
+
+	if rowsAffected <= 0 {
+		return uint64(0), errors.New("not found note")
+	}
+
+	return uint64(rowsAffected), nil
 }
 
 func (r *repo) DescribeNote(ctx context.Context, id uint64) (*note.Note, error) {
